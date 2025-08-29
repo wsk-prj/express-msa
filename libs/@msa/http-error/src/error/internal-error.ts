47c4461c9f183144a843138e50369c14c868888a
wsk0715 <leindey@gmail.com>
2025-08-29 16:18:25 +0900

    separate http errors to @msa/http-error

---
+++ libs/@msa/http-error/src/error/internal-error.ts
@@ -0,0 +1,10 @@
+import { HttpError } from "./http-error";
+
+export class InternalServerError extends HttpError {
+  constructor(details?: any) {
+    super();
+    this.status = 500;
+    this.message = "Internal Server Error";
+    this.details = details;
+  }
+}
