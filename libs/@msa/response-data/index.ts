04bfa072ce87cfefb12dfa243f3b029a6af0c00a
wsk0715 <leindey@gmail.com>
2025-08-29 16:37:52 +0900

    separate data response module to @msa/response-data

---
+++ libs/@msa/response-data/index.ts
@@ -0,0 +1,4 @@
+export * from "./src/types/express.type";
+
+export { ApiData } from "./src/api/api-data";
+export { responseDataHandler } from "./src/middlewares/response-data.handler";
---
+++ libs/@msa/response-data/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types/express.type";
+
+export { ApiData } from "./api/api-data";
+
+export { responseDataHandler } from "./middlewares/response-data.handler";
+export { errorHandler } from "./middlewares/error.handler";
