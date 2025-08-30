06c40fc27642a02357ba9647bfc6e66f0d3dd6c4
wsk0715 <leindey@gmail.com>
2025-08-30 23:04:04 +0900

    fix @msa/response-data build error

--- libs/@msa/response-data/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./src/types/express.type";
-
-export { Page, PagedRequest } from "./src/api/pagination/page";
-export { ApiData } from "./src/api/api-data";
-
-export { responseDataHandler } from "./src/middlewares/response-data.handler";
-
-export { createPage } from "./src/utils/page.util";
--- libs/@msa/response-data/src/index.ts
+++ libs/@msa/response-data/src/index.ts
@@ -1,6 +1,9 @@
 export * from "./types/express.type";
 
+export { Page, PagedRequest } from "./api/pagination/page";
 export { ApiData } from "./api/api-data";
 
 export { responseDataHandler } from "./middlewares/response-data.handler";
 export { errorHandler } from "./middlewares/error.handler";
+
+export { createPage } from "./utils/page.util";
