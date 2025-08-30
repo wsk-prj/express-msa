3a7dea26bf034314e9c3113273850a2f2ac9bc77
wsk0715 <leindey@gmail.com>
2025-08-30 23:52:48 +0900

    add paged request schema to @msa/shared

--- libs/@msa/shared/src/index.ts
+++ libs/@msa/shared/src/index.ts
@@ -6,3 +6,6 @@ export { EventBus, eventBus } from "./services/event-bus";
 
 // Middlewares
 export { validateRequest, validateQuery } from "./middlewares/dto.validator";
+
+// Schemas
+export { PagedRequestSchema, PagedRequest } from "./schema/request.schema";
