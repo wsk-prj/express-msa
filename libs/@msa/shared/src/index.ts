775d6db6d90e75afe6897135dd9bf56ffad80fae
wsk0715 <leindey@gmail.com>
2025-08-30 23:36:40 +0900

    add request query validation on @msa/shared

--- libs/@msa/shared/src/index.ts
+++ libs/@msa/shared/src/index.ts
@@ -5,4 +5,4 @@ export * from "./types/events.type";
 export { EventBus, eventBus } from "./services/event-bus";
 
 // Middlewares
-export { validateRequest } from "./middlewares/dto.validator";
+export { validateRequest, validateQuery } from "./middlewares/dto.validator";
--- libs/@msa/shared/src/middlewares/dto.validator.ts
+++ libs/@msa/shared/src/middlewares/dto.validator.ts
@@ -7,9 +7,8 @@ import { BadRequestError } from "@msa/http-error";
  * Zod 스키마를 사용한 요청 검증 미들웨어
  */
 export function validateRequest(schema: ZodSchema) {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
     try {
-      // Zod 스키마 검증
       const validatedData = schema.parse(req.body);
       req.body = validatedData;
 
@@ -23,6 +22,29 @@ export function validateRequest(schema: ZodSchema) {
       }
 
       throw new BadRequestError("Invalid request data");
+    }
+  };
+}
+
+/**
+ * Zod 스키마를 사용한 쿼리 파라미터 검증 미들웨어
+ */
+export function validateQuery(schema: ZodSchema) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    try {
+      const validatedData = schema.parse(req.query);
+      req.query = validatedData;
+
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const firstError = error.errors[0];
+        const errorMessage = firstError?.message || "Query validation failed";
+
+        throw new BadRequestError(errorMessage);
+      }
+
+      throw new BadRequestError("Invalid query parameters");
     }
   };
 }
